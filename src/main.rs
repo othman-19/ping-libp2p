50973485ce8e92c749890c953134326b8a04ea87
othman-19 <othmanenaamani@gmail.com>
2023-09-05 07:09:57 +0100

    Created network behavior

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use libp2p::{identity, PeerId, development_transport};
+use libp2p::{identity, PeerId, ping, development_transport};
+use libp2p::swarm::{keep_alive, NetworkBehaviour};
 use std::error::Error;
+
+/// Our network behaviour.
+///
+/// For illustrative purposes, this includes the [`KeepAlive`](behaviour::KeepAlive) behaviour
+/// so a continuous sequence of pings can be observed.
+#[derive(NetworkBehaviour, Default)]
+struct Behaviour {
+    keep_alive: keep_alive::Behaviour,
+    ping: ping::Behaviour,
+}
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -8,6 +19,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     println!("Local peer id: {local_peer_id:?}");
 
     let transport = development_transport(local_key).await?;
+
+    let behaviour = Behaviour::default();
 
     Ok(())
 }
