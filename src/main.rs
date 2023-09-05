9ca9ac427b79f6964423fec9cff50d6eb226728e
othman-19 <othmanenaamani@gmail.com>
2023-09-05 06:53:55 +0100

    Added Network node identity and a Transport

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use libp2p::{identity, PeerId, development_transport};
+use std::error::Error;
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {local_peer_id:?}");
+
+    let transport = development_transport(local_key).await?;
+
+    Ok(())
 }
